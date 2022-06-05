3e0ff19a20a05db4c0adbb8f3c74c5c27fa69396
Jhost <jhost94@gmail.com>
2022-06-05 21:12:07 +0000

    Removed message managing responsability.

--- src/main/java/com/jhost/core/Core/service/TestService.java
+++ src/main/java/com/jhost/core/Core/service/TestService.java
@@ -1,7 +1,6 @@
 package com.jhost.core.Core.service;
 
 import com.jhost.core.Core.config.Constants;
-import com.jhost.core.Core.service.meta.MessageService;
 import org.springframework.stereotype.Service;
 
 import java.util.Locale;
@@ -9,13 +8,7 @@ import java.util.Locale;
 @Service
 public class TestService {
 
-    private final MessageService messageService;
-
-    public TestService(MessageService messageService) {
-        this.messageService = messageService;
-    }
-
     public String getTestMessage(Locale locale){
-        return messageService.getMessage(Constants.MessagePaths.TEST_MESSAGE, locale);
+        return Constants.MessagePaths.TEST_MESSAGE.getMessage();
     }
 }
